9a4c9a1113f63f783207c7ce5188f1de2a4aaea4
San4o <san4ousoff@mail.ru>
2024-01-11 12:23:55 +0200

    delete file with version 0.01, add project for version 0.01

--- CommandLineVersion001/CommandLineVersion001/main.swift
+++ CommandLineVersion001/CommandLineVersion001/main.swift
@@ -1,6 +1,6 @@
 //
-//  version001.swift
-//  
+//  main.swift
+//  CommandLineVersion001
 //
 //  Created by Mac on 11.01.2024.
 //
@@ -39,16 +39,16 @@ func findBestCard(for placePay: PlacePay, from cards: [Card]) -> Card? {
     return bestCard
 }
 
-let placePay = PlacePay(id: 1, name: "Ресторан", icon: "restaurant_icon")
-let placePay = PlacePay(id: 2, name: "Супермаркет", icon: "restaurant_icon")
+let restaurantPay = PlacePay(id: 1, name: "Ресторан", icon: "restaurant_icon")
+let supermarketPay = PlacePay(id: 2, name: "Супермаркет", icon: "supermarket_icon")
 
 let cards = [
-    Card(id: 1, name: "Card1", placePay: [placePay], cashbackPercentage: 5.0, icon: "card_icon1"),
-    Card(id: 2, name: "Card2", placePay: [placePay], cashbackPercentage: 7.5, icon: "card_icon2"),
-    Card(id: 3, name: "Card3", placePay: [placePay], cashbackPercentage: 3.0, icon: "card_icon3")
+    Card(id: 1, name: "Card1", placePay: [restaurantPay], cashbackPercentage: 5.0, icon: "card_icon1"),
+    Card(id: 2, name: "Card2", placePay: [restaurantPay], cashbackPercentage: 7.5, icon: "card_icon2"),
+    Card(id: 3, name: "Card3", placePay: [restaurantPay], cashbackPercentage: 3.0, icon: "card_icon3")
 ]
 
-if let bestCard = findBestCard(for: placePay, from: cards) {
+if let bestCard = findBestCard(for: restaurantPay, from: cards) {
     print("Выбрана карточка \(bestCard.name) с процентом кэшбека \(bestCard.cashbackPercentage)%")
 } else {
     print("Не удалось найти подходящую карточку для данного места оплаты")
